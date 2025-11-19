d488ad25dda285b291efc76fd153e2b3e92bab37
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-19 14:34:28 +0530

    first commit

---
+++ src/main/java/uwu/connectra/connectra_backend/ConnectraBackendApplication.java
@@ -0,0 +1,13 @@
+package uwu.connectra.connectra_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uwu/connectra/connectra_backend/ConnectraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uwu.connectra.connectra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
