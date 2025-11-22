a6c32140bee99376538c93a86d9d6c50dcdc8bf5
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-22 22:55:32 +0530

    Add Student entity with fields for first name, last name, degree, batch, email, hashed password, and profile photo URL

---
+++ src/main/java/uwu/connectra/connectra_backend/entities/Student.java
@@ -0,0 +1,37 @@
+package uwu.connectra.connectra_backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Table(name="students")
+@Getter
+@Setter
+@Entity
+public class Student extends User{
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "degree")
+    private String degree;
+
+    @Column(name = "batch")
+    private int batch;
+
+    @Column(name = "student_email")
+    private String student_email;
+
+    @Column(name = "hashed_password")
+    private String hashed_password;
+
+    @Column(name = "profile_photo_url")
+    private String profile_photo_url = "https://placehold.co/100x100"; //Default URL
+}
