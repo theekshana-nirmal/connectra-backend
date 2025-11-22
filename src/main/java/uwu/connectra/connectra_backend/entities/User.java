575032ec6513405117eddf9cff683f79bae58a30
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-22 23:10:50 +0530

    Refactor User entity: add missing imports and annotations for auditing

--- src/main/java/uwu/connectra/connectra_backend/entities/User.java
+++ src/main/java/uwu/connectra/connectra_backend/entities/User.java
@@ -1,5 +1,9 @@
 package uwu.connectra.connectra_backend.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -8,6 +12,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -15,12 +20,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
