808038b78e5179d387a953509a5fd9c68b159fab
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-23 10:48:00 +0530

    Refactor Admin entity: add comments to clarify default role assignment logic

--- src/main/java/uwu/connectra/connectra_backend/entities/Admin.java
+++ src/main/java/uwu/connectra/connectra_backend/entities/Admin.java
@@ -11,6 +11,10 @@ import lombok.Setter;
 @Entity
 @Table(name = "admins")
 public class Admin extends User{
+    // NOTE: This sets the default role to ADMIN before saving,
+    // But assigning roles in the service layer is the recommended best practice.
+    // In this specific case, it is acceptable because the role for Admin is fixed and will never change dynamically.
+    // Same thing applied to Lecturers and Students
     @PrePersist
     protected void onCreate(){
         if(this.getRole() == null){
