936a090f5e438f4fa756bd80518e3982776153cd
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-23 11:36:26 +0530

    Remove unnecessary imports and annotations from AdminRepository and BaseRepository

--- src/main/java/uwu/connectra/connectra_backend/repositories/AdminRepository.java
+++ src/main/java/uwu/connectra/connectra_backend/repositories/AdminRepository.java
@@ -1,6 +1,5 @@
 package uwu.connectra.connectra_backend.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uwu.connectra.connectra_backend.entities.Admin;
 
--- src/main/java/uwu/connectra/connectra_backend/repositories/BaseRepository.java
+++ src/main/java/uwu/connectra/connectra_backend/repositories/BaseRepository.java
@@ -1,8 +1,6 @@
 package uwu.connectra.connectra_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
 }
