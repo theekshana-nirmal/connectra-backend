b21120f6b65e0b27267b83af1a6bd561d0a33966
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-11-23 11:38:39 +0530

    Add NoRepositoryBean annotation to BaseRepository to prevent Spring instantiation

--- src/main/java/uwu/connectra/connectra_backend/repositories/BaseRepository.java
+++ src/main/java/uwu/connectra/connectra_backend/repositories/BaseRepository.java
@@ -1,6 +1,8 @@
 package uwu.connectra.connectra_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean // Prevents Spring from trying to create an instance
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
 }
