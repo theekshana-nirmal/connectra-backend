8023651f3a7c3ef740265c3ca1ce7fcac2a73160
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-04 16:13:20 +0530

    Add CustomUserDetailsService to load user details by email

---
+++ src/main/java/uwu/connectra/connectra_backend/services/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package uwu.connectra.connectra_backend.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import uwu.connectra.connectra_backend.entities.CustomUserDetails;
+import uwu.connectra.connectra_backend.entities.User;
+import uwu.connectra.connectra_backend.repositories.UserRepository;
+
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User with email " + email + " not found")
+                );
+
+        return new CustomUserDetails(user);
+    }
+}
