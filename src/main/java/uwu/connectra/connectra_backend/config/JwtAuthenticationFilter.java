a2fbe9a87caf1c50bb9756acc4584f96d09997e1
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-05 12:27:06 +0530

    Add JwtAuthenticationFilter to handle JWT token validation and user authentication

---
+++ src/main/java/uwu/connectra/connectra_backend/config/JwtAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package uwu.connectra.connectra_backend.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import uwu.connectra.connectra_backend.services.CustomUserDetailsService;
+import uwu.connectra.connectra_backend.services.JwtService;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    JwtService jwtService;
+    CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        // Get the JWT token from the Authorization header
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Extract the token and email
+        String jwt = authHeader.substring(7);
+        String tokenEmail = jwtService.extractEmail(jwt);
+
+        // Validate the token and set authentication if valid
+        if (tokenEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            var userDetails = customUserDetailsService.loadUserByUsername(tokenEmail);
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                // Make spring security object that represents the logged-in user
+                var authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
