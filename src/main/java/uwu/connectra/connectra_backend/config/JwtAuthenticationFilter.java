48e45bc9ff0609948b16ab2e867d2b4e4cbae4ff
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-05 12:46:25 +0530

    Add null checks for HttpServletRequest, HttpServletResponse, and FilterChain in JwtAuthenticationFilter

--- src/main/java/uwu/connectra/connectra_backend/config/JwtAuthenticationFilter.java
+++ src/main/java/uwu/connectra/connectra_backend/config/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -18,9 +19,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
         // Get the JWT token from the Authorization header
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
