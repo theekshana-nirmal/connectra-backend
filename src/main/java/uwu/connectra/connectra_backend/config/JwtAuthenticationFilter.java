b317d2166b053c708afc4250a6239ebf3261d1e2
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-05 13:14:01 +0530

    Refactor JwtAuthenticationFilter to use constructor injection and add component annotation

--- src/main/java/uwu/connectra/connectra_backend/config/JwtAuthenticationFilter.java
+++ src/main/java/uwu/connectra/connectra_backend/config/JwtAuthenticationFilter.java
@@ -5,17 +5,21 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import uwu.connectra.connectra_backend.services.CustomUserDetailsService;
 import uwu.connectra.connectra_backend.services.JwtService;
 
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    JwtService jwtService;
-    CustomUserDetailsService customUserDetailsService;
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal(
