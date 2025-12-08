e3487f3dbf9f6410035e9193aebe1d8d3b09c295
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-08 22:37:54 +0530

    Remove unused import for Role in UserRegisterRequestDTO

--- src/main/java/uwu/connectra/connectra_backend/dtos/UserRegisterRequestDTO.java
+++ src/main/java/uwu/connectra/connectra_backend/dtos/UserRegisterRequestDTO.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import uwu.connectra.connectra_backend.entities.Role;
 
 @AllArgsConstructor
 @NoArgsConstructor
