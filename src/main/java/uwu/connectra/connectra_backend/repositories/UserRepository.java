002a2fb255c7fdb100c3486cb4fff1ad18a59eba
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-09 11:58:54 +0530

    Remove unused NoRepositoryBean import from UserRepository

--- src/main/java/uwu/connectra/connectra_backend/repositories/UserRepository.java
+++ src/main/java/uwu/connectra/connectra_backend/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package uwu.connectra.connectra_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 import uwu.connectra.connectra_backend.entities.Role;
 import uwu.connectra.connectra_backend.entities.User;
