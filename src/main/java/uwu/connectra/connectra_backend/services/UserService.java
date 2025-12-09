94a754fb962abd037d8ee87fa90f3109318a47ba
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-09 11:16:32 +0530

    Add method to retrieve all lecturers in UserService

--- src/main/java/uwu/connectra/connectra_backend/services/UserService.java
+++ src/main/java/uwu/connectra/connectra_backend/services/UserService.java
@@ -3,8 +3,12 @@ package uwu.connectra.connectra_backend.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import uwu.connectra.connectra_backend.dtos.LecturerResponseDTO;
+import uwu.connectra.connectra_backend.entities.Role;
 import uwu.connectra.connectra_backend.exceptions.UserNotFoundException;
 import uwu.connectra.connectra_backend.repositories.UserRepository;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -21,5 +25,19 @@ public class UserService {
         }
         userRepository.deleteById(userId);
         log.info("User with ID {} deleted successfully", userId);
+    }
+
+    // Return All Lecturers
+    public List<LecturerResponseDTO> getAllLecturers() {
+        log.info("Fetching all lecturers from the database");
+        var lecturers = userRepository.findAllByRole(Role.LECTURER);
+        return lecturers.stream()
+                .map(lecturer -> new LecturerResponseDTO(
+                        lecturer.getId(),
+                        lecturer.getFirstName(),
+                        lecturer.getLastName(),
+                        lecturer.getEmail()
+                ))
+                .toList();
     }
 }
