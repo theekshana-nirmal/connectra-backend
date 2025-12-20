a79e414af3a9022134bc1411b16c5cf1f88dac6a
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-20 16:18:41 +0530

    Add validation constraints to AgoraConfig properties

--- src/main/java/uwu/connectra/connectra_backend/config/AgoraConfig.java
+++ src/main/java/uwu/connectra/connectra_backend/config/AgoraConfig.java
@@ -1,5 +1,7 @@
 package uwu.connectra.connectra_backend.config;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -10,7 +12,12 @@ import org.springframework.context.annotation.Configuration;
 @Getter
 @Setter
 public class AgoraConfig {
+    @NotBlank(message = "Agora App ID must not be blank")
     private String appId;
+
+    @NotBlank(message = "Agora App Certificate must not be blank")
     private String appCertificate;
+
+    @Min(value = 60, message = "Token expire time must be at least 60 second")
     private int tokenExpireTime;
 }
