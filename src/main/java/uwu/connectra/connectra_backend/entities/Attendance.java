e375367ab78a84de02102629c36a9183e3d234f7
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-23 00:49:07 +0530

    Add Attendance entity to track student attendance details

---
+++ src/main/java/uwu/connectra/connectra_backend/entities/Attendance.java
@@ -0,0 +1,61 @@
+package uwu.connectra.connectra_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "joined_at")
+    private LocalDateTime joinedAt;
+
+    @Column(name = "last_joined_at")
+    private LocalDateTime lastJoinedAt;
+
+    @Column(name = "left_at")
+    private LocalDateTime leftAt;
+
+    @Column(name = "total_duration_in_minutes")
+    private Integer totalDurationInMinutes;
+
+    @Column(name = "attendance_percentage")
+    private Double attendancePercentage;
+
+    @Column(name = "attendance_status")
+    @Enumerated(EnumType.STRING)
+    private AttendanceStatus attendanceStatus;
+
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    // Relationship fields
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "meeting_id", nullable = false)
+    private Meeting meetingId;
+}
