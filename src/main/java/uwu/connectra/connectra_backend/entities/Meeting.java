143dfdaf3ef55ac91b961486c20eebe1e5a0ccbf
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-23 00:48:57 +0530

    Add attendance relationship to Meeting entity

--- src/main/java/uwu/connectra/connectra_backend/entities/Meeting.java
+++ src/main/java/uwu/connectra/connectra_backend/entities/Meeting.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -74,4 +75,7 @@ public class Meeting {
     @ManyToOne
     @JoinColumn(name = "lecturer_id", nullable = false)
     private Lecturer createdBy;
+
+    @OneToMany(mappedBy = "meetingId", cascade = CascadeType.ALL)
+    private List<Attendance> attendances;
 }
