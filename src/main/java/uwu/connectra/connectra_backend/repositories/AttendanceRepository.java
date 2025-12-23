757d5a6e92ba5dc14e017101dce676c2bfc2b8f4
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-24 01:52:44 +0530

    Add methods to AttendanceRepository for retrieving attendance records by meeting and status

--- src/main/java/uwu/connectra/connectra_backend/repositories/AttendanceRepository.java
+++ src/main/java/uwu/connectra/connectra_backend/repositories/AttendanceRepository.java
@@ -3,14 +3,21 @@ package uwu.connectra.connectra_backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uwu.connectra.connectra_backend.entities.Attendance;
+import uwu.connectra.connectra_backend.entities.AttendanceStatus;
 import uwu.connectra.connectra_backend.entities.Meeting;
 import uwu.connectra.connectra_backend.entities.Student;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     // Find by Student and Meeting
     Optional<Attendance> findByStudentAndMeeting(Student student, Meeting meeting);
+
+    // Get all Attendance records for a Meeting
+    List<Attendance> findAllByMeeting(Meeting meeting);
+
+    // Get students by attendance status for a specific meeting
+    List<Attendance> findAllByMeetingAndStatus(Meeting meeting, AttendanceStatus status);
 }
