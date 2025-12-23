a4edccaa2bf9219a69616180a50226f59adcfefd
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2025-12-24 01:52:35 +0530

    Add detailed attendance report DTOs and student attendance records

--- src/main/java/uwu/connectra/connectra_backend/dtos/AttendanceReportResponseDTO.java
+++ src/main/java/uwu/connectra/connectra_backend/dtos/AttendanceReportResponseDTO.java
@@ -5,14 +5,38 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class AttendanceReportResponseDTO {
+    // --- 1. Meeting Metadata ---
     private String meetingId;
-    private String meetingTopic;
-    private int totalParticipants;
-    private int presentParticipants;
-    private int absentParticipants;
+    private String topic;
+    private String lecturerName;
+    private String degree;
+    private int batch;
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private int durationMinutes;
+
+    // --- 2. Summary Statistics (The Numbers) ---
+    private int totalStudents; // In target degree and batch
+    private int totalParticipated;
+    private int presentCount;
+    private int partialCount;
+    private int absentCount;
+
+    // --- 3. Detailed Attendance Records ---
+    // All present students (Students who attended more than or equal to 80% of the meeting duration)
+    List<StudentAttendanceDTO> presentStudents;
+
+    // All partially attended students
+    List<StudentAttendanceDTO> partiallyAttendedStudents;
+
+    // All absent students
+    List<StudentAttendanceDTO> absentStudents;
 }
